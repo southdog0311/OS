11a79170d8eeb8f409dda51e35958fbd65d76065
southdog0311 <75650097+southdog0311@users.noreply.github.com>
2023-02-01 20:46:27 +0800

    Add files via upload
    simple shell

---
+++ os-shell/child.c
@@ -0,0 +1,35 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+char input;
+
+void *mythread(void *vargp) {
+  while (1) {
+    input = getchar();
+    sleep(500);
+  }
+  return NULL;
+}
+
+int main() {
+  int cnt = 1, start_time, end_time;
+
+  end_time = start_time = time(NULL);
+  pthread_t thread_id;
+  pthread_create(&thread_id, NULL, mythread, NULL);
+  while (1) {
+    if (input == 'q')
+      break;
+    if (difftime(end_time, start_time) >= 3) {
+      printf("child currently running, ");
+      printf("pid: %d\n", getpid());
+      printf("%d continue, Type q to quit\n", cnt++);
+      start_time = time(NULL);
+    }
+    end_time = time(NULL);
+  }
+}
---
+++ os-shell/my_shell.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int shellcmd(char input[]) {
+  if (strcmp(input, "tree") == 0) {
+    system("tree");
+    return 1;
+  }
+  if (strcmp(input, "sort") == 0) {
+    system("sort");
+    return 1;
+  }
+  if (strcmp(input, "clear") == 0) {
+    system("clear");
+    return 1;
+  }
+  if (strcmp(input, "exit") == 0) {
+    exit(0);
+    return 1;
+  }
+  if (strcmp(input, "pwd") == 0) {
+    system("pwd");
+    return 1;
+  }
+  if (strcmp(input, "ls") == 0) {
+    system("ls");
+    return 1;
+  }
+  if (strcmp(input, "ps axe") == 0) {
+    system("ps axe");
+    return 1;
+  } else
+    return 0;
+}
+
+int childcmd(char input[]) {
+  if (strcmp(input, "child") == 0)
+    return 1;
+  else if (strcmp(input, "child &") == 0)
+    return 1;
+  else
+    return 0;
+}
+
+int main(void) {
+  int pid;
+  char *args[] = {"./child", NULL};
+  char inputs[10];
+  char child_cmd[] = "child";
+  char parent[] = "child &";
+  printf("parent pid is: %d\n", getpid());
+  while (1) {
+    printf("1092929_my_shell>");
+    fgets(inputs, sizeof(inputs), stdin);
+    inputs[strcspn(inputs, "\n")] = '\0';
+    if (shellcmd(inputs)) {
+      continue;
+    }
+    if (childcmd(inputs)) {
+      pid = fork();
+    }
+    if (pid == 0 && childcmd(inputs)) {
+      int msg = execvp(args[0], args);
+      printf("exec() error: %d\n", msg);
+    } else if (pid > 0 && childcmd(inputs)) {
+      if (strcmp(inputs, parent) != 0)
+        wait(NULL);
+      printf("back to parent process\n");
+    } else {
+      printf("wrong commend\n");
+    }
+  }
+}
